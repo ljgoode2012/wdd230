7c1398d0f308d75f5135f3964d324eb80299e6e8
ljgoode2012 <ljgoode2012@gmail.com>
2022-10-27 21:02:04 -0400

    lazy loading and hover

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -27,3 +27,26 @@ let day = date.getDay();
 console.log(`day ${day}`)
 if ((day === 1) || (day === 2)) {banner.style.display = "block"}
 else {banner.style.display = "none"}
+
+// lazy loading
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    }
+}
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        })
+    })
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    })
+}
