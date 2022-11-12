6e07ad102ed19f8e10b45c8131eaf173ad950dff
ljgoode2012 <ljgoode2012@gmail.com>
2022-11-11 23:44:52 -0500

    updated json and finished script

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,41 @@
+const url = "https://ljgoode2012.github.io/wdd230/chamber/data/data.json"
+
+const companies = fetchJSON(url)
+console.log(companies)
+
+
+async function fetchJSON(resource) {
+    const response = await fetch(resource)
+    if (response.ok) {
+        const companies = await response.json();
+        companies.forEach(company => displayCompanyCards(company))}
+}
+
+function displayCompanyCards (company) {
+    const cards = document.querySelector(".cards")
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phoneNumber = document.createElement('p');
+    let website = document.createElement('a');
+
+    
+    logo.setAttribute("src", `${company.imageFile}`)
+    logo.setAttribute("style", "width:100%")
+    logo.setAttribute("alt", `${company.name} logo`)
+    logo.setAttribute("loading", "lazy")
+    website.setAttribute("href", `${company.websiteURL}`)
+    address.textContent = company.address
+    phoneNumber.textContent = company.phoneNumber
+    website.textContent = company.websiteURL
+
+    card.append(logo)
+    card.append(address)
+    card.append(phoneNumber)
+    card.append(website)
+
+    cards.appendChild(card)
+}
+
+
+
