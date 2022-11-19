fbe13db16a54df8debc504ce044d9bbac27e4eac
ljgoode2012 <ljgoode2012@gmail.com>
2022-11-19 01:10:29 -0500

    set up weather api

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,17 +1,58 @@
-let temp = parseInt(document.querySelector("#temp").textContent)
-let wSpeed = parseInt(document.querySelector("#wSpeed").textContent)
-let wChillElement = document.querySelector("#wChill")
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=41.6662&lon=81.3396&units=imperial&appid=e2e30e7d15e743523fc54c1bbdfcb1be";
+temperatureEl = document.querySelector("#temp")
+conditionEl = document.querySelector("#condition")
+wSpeedEl = document.querySelector("#wSpeed")
+wChillEl = document.querySelector("#wChill")
+iconEl = document.querySelector("#icon")
 
-calWindchill()
+weatherFetch()
 
-function calWindchill() {
-    console.log("entered function")
-    if ((temp <= 50) && (wSpeed > 3)) {
-        wChill = Math.round(35.74 + (0.6215 * temp) - (35.75 * (wSpeed**.16)) + (0.4275 * temp * (wSpeed**.16)))
-        console.log(wChill)
-        wChillElement.textContent = wChill
+async function weatherFetch() {
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data)
+            array = pullResults(data);
+            setElements(array);
+
+        }
+    } catch(error) {console.log(`Error: ${error.message}`);}
+}
+
+function pullResults(weatherData) {
+    let temperature = weatherData.main.temp
+    let wSpeed = weatherData.wind.speed
+    let wChillResult = calWindchill(temperature, wSpeed)
+    let condition = capitalize(weatherData.weather[0].description);
+    let icon = weatherData.weather[0].icon
+    return [temperature, wSpeed, wChillResult, condition, icon]
+}
+ 
+function setElements(array) {
+    temperatureEl.textContent = array[0]
+    wSpeedEl.textContent = array[1]
+    wChillEl.textContent = array[2]
+    conditionEl.textContent = array[3]
+    iconEl.setAttribute("src", `http://openweathermap.org/img/wn/${array[4]}@2x.png`)
+    iconEl.alt = array[3]
+}
+
+function calWindchill(temperature, wSpeed) {
+    if ((temperature <= 50) && (wSpeed > 3)) {
+        wChill = Math.round(35.74 + (0.6215 * temperature) - (35.75 * (wSpeed**.16)) + (0.4275 * temperature * (wSpeed**.16)))
+        return wChill
     }
     else {
-        wChillElement.textContent = "N/A";
+        return "N/A"
     }
+}
+
+function capitalize(string) {
+    let words = string.split(" ");
+    const newWords = words.map(function (word) {
+        let newWord = `${word.charAt(0).toUpperCase()}${word.slice(1)}`
+        return newWord})
+    let newString = newWords.join(" ")
+    return newString
 }
